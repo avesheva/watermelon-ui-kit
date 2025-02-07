76310bf832236e3c70fc907f627017bb9e3ab084
avesheva <shevochkandrey@gmail.com>
2025-02-07 23:44:26 +0200

    Init commit

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  lib: {
+    entry: resolve(__dirname, 'src/components/index.ts'),
+    name: 'watermelon-ui-kit',
+    fileName: (format) => `watermelon-ui-kit.${ format }.ts`,
+  },
+  rollupOptions: {
+    external: ['vue'],
+    output: {
+      globals: {
+        vue: 'Vue',
+      },
+    },
+  },
+})
