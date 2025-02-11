3b8a8eabb48c6e5748f04ef0929931c4ca1ee894
avesheva <shevochkandrey@gmail.com>
2025-02-11 23:50:11 +0200

    Added tests dependencies and GitHub actions workflow

---
+++ src/components/Tag/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <h3>Tag. Just tag</h3>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Tag/tag.test.ts
@@ -0,0 +1,5 @@
+import { expect, test } from 'vitest'
+
+test('Just tag test', () => {
+  console.log('TAG test. Just tag test...')
+})
