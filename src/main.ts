d82c4e3ac942867e6b21ffc7742eafa66b849e9b
avesheva <shevochkandrey@gmail.com>
2025-03-12 23:36:24 +0200

    Added 'vue-router'

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import PrimeVue from 'primevue/config'
 import './style.css'
 import App from './App.vue'
 
+const router = createRouter({ history: createWebHistory(), routes: [] })
+
 const app = createApp(App)
 app.use(PrimeVue, { theme: 'none' })
+app.use(router)
 
 app.mount('#app')
