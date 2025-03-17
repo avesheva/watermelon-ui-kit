8c1c70f3edb82d329374c20cdaf3332ac66be223
avesheva <shevochkandrey@gmail.com>
2025-03-17 23:19:36 +0200

    Fix build styles

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,15 @@
 <script setup lang="ts">
+import { Button } from 'primevue'
+
+import WheelLoader from './components/WheelLoader/index.vue'
+import Tag from './components/Tag/index.vue'
+
+const tag = {
+  id: 1,
+  name: 'Learning',
+  status: 2,
+  rate: 0.24,
+}
 
 </script>
 
@@ -6,6 +17,13 @@
   <div>
    <h3>Watermelon UI kit</h3>
    <h4>Library items development area</h4>
+
+    <Tag :tag="tag" closable show-rate />
+
+    <Button
+      label="Send +"
+      class="p-3 w-3"
+    />
   </div>
 </template>
 
