bebb1bfa67ee20e85cfc6c543358b204e7645b32
avesheva <shevochkandrey@gmail.com>
2025-06-11 22:15:09 +0300

    Update 'TagType' type

--- src/components/Tag/types.ts
+++ src/components/Tag/types.ts
@@ -1,9 +1,15 @@
 import { TAG_STATUS } from '@/components/Tag/constants.ts'
 
 export type TagType = {
+  id: string,
   name: string,
   status: typeof TAG_STATUS.NEW | typeof TAG_STATUS.SYSTEM | typeof TAG_STATUS.PUBLISHED,
-  id?: string,
   rate?: number,
-  tagId?: string,
+  tagId?: never,
+} | {
+  tagId: string,
+  name: string,
+  status: typeof TAG_STATUS.NEW | typeof TAG_STATUS.SYSTEM | typeof TAG_STATUS.PUBLISHED,
+  id?: never,
+  rate?: number,
 }
